51a30a3661e05ae4def70500a83111ccbf64c359
natelant <natelant@gmail.com>
2024-11-14 17:58:20 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# This file hosts the main application using streamlit.
+
+import streamlit as st
+
+st.title("ATSPM Signal Operations Dashboard")
+
---
+++ fetch.py
@@ -0,0 +1,6 @@
+# This file will fetch the data from the ATSPM API and organize into a pandas dataframe.
+
+import pandas as pd
+import requests
+import json
+
